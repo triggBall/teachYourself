0ff25b7d532eac4f75438ee068eb3cde59b035b7
triggBall <trigg.allen@ballaerospace.com>
2022-02-03 13:54:10 -0700

    finished chapter 3

--- teachYourself/Chapter3.cpp
+++ teachYourself/Chapter3.cpp
@@ -7,11 +7,34 @@ int secondNumber = 0;
 int multiplicationResult = 0; 
 
 void MultiplyNumbers();
+void ShowEnumeration(); 
 
 int Chapter3()
 {
+	ShowEnumeration();
 	MultiplyNumbers(); 
 	return 0; 
+}
+
+enum CardinalDirections
+{
+	North = 25,
+	South,
+	East,
+	West
+
+};
+
+void ShowEnumeration()
+{
+	std::cout << "Displaying directions and their symbolic values:" << std::endl; 
+	std::cout << "North: " << North << std::endl; 
+	std::cout << "South: " << South << std::endl; 
+	std::cout << "East: " << East << std::endl; 
+	std::cout << "West: " << West << std::endl; 
+
+	CardinalDirections windDirection = South; 
+	std::cout << "Variable windDirection = " << windDirection << std::endl;
 }
 
 void MultiplyNumbers()
